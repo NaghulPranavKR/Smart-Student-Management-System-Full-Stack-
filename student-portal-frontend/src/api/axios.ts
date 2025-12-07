e289759564d199c5c042dc2c87f82bd7165b373d
NaghulPranavKR <167752998+NaghulPranavKR@users.noreply.github.com>
2025-12-07 13:33:36 +0530

    Configured axios for production

--- student-portal-frontend/src/api/axios.ts
+++ student-portal-frontend/src/api/axios.ts
@@ -1,5 +1,7 @@
-import axios from 'axios';
+import axios from "axios";
+
+const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
 
 export default axios.create({
-  baseURL: 'http://localhost:5000'  // backend URL
+  baseURL: API_URL
 });
